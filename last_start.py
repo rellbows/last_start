22984903fe94c36fbecca60a71794802f5cf2b63
rellbows <ryan.ellis927@gmail.com>
2018-04-28 16:11:27 -0500

    Implementation of the 'last-to-start' activity selector function. Has been tested out succesfully with lists of 0, 1, and 5 activities.

--- last_start.py
+++ last_start.py
@@ -18,10 +18,34 @@ def build_lists():
 
 # algorithm
 
-def last_start():
+def last_start(act_list):
 	'''last-to-start activity selection implementation'''
+
+	# list that will hold optimal activities for the activity list
+	act_results = []
+
+	# check to make sure there are activities in list
+	if len(act_list) > 0:
+
+		# add last activity to results list
+		act_results.append(act_list[0][0])
+
+		# travelers used to iterate through the activity list
+		i = 0
+		j = 1
+
+		# iterate through list checking i's start time vs. j's finish time
+		while(j < len(act_list)):
+			if act_list[i][1] >= act_list[j][2]:
+				act_results.append(act_list[j][0])
+				i = j
+			j += 1
+
+	return act_results
 
 def main():
 	'''driver code'''
+	
+
 
 main()
